ef9ad343a4ec4637a6ffec6787c147491f3cfdb7
Olaf Lüke <olaf@tinkerforge.com>
2015-04-23 14:36:39 +0200

    Add Python examples

---
+++ software/examples/python/example_simple.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-  
+
+HOST = "localhost"
+PORT = 4223
+UID = "ABC" # Change to your UID
+
+from tinkerforge.ip_connection import IPConnection
+from tinkerforge.bricklet_analog_in_v2 import AnalogInV2
+
+if __name__ == "__main__":
+    ipcon = IPConnection() # Create IP connection
+    ai = AnalogInV2(UID, ipcon) # Create device object
+
+    ipcon.connect(HOST, PORT) # Connect to brickd
+    # Don't use device before ipcon is connected
+
+    # Get current voltage (unit is mV)
+    voltage = ai.get_voltage()
+
+    print('Voltage: ' + str(voltage/1000.0) + 'V')
+
+    raw_input('Press key to exit\n') # Use input() in Python 3
+    ipcon.disconnect()
