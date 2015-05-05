d3cbb5ad92ed0ab5ebb81388a7a59f6d9e8ead48
Olaf Lüke <olaf@tinkerforge.com>
2015-05-05 10:19:20 +0200

    Add C# examples

---
+++ software/examples/csharp/ExampleCallback.cs
@@ -0,0 +1,35 @@
+using Tinkerforge;
+
+class Example
+{
+	private static string HOST = "localhost";
+	private static int PORT = 4223;
+	private static string UID = "ABC"; // Change to your UID
+
+	// Callback function for voltage callback (parameter has unit mV)
+	static void VoltageCB(BrickletAnalogInV2 sender, int voltage)
+	{
+		System.Console.WriteLine("Voltage: " + voltage/1000.0 + " V");
+	}
+
+	static void Main() 
+	{
+		IPConnection ipcon = new IPConnection(); // Create IP connection
+		BrickletAnalogInV2 ai = new BrickletAnalogInV2(UID, ipcon); // Create device object
+
+		ipcon.Connect(HOST, PORT); // Connect to brickd
+		// Don't use device before ipcon is connected
+
+		// Set Period for voltage callback to 1s (1000ms)
+		// Note: The voltage callback is only called every second if the 
+		//       voltage has changed since the last call!
+		ai.SetVoltageCallbackPeriod(1000);
+
+		// Register voltage callback to function VoltageCB
+		ai.Voltage += VoltageCB;
+
+		System.Console.WriteLine("Press enter to exit");
+		System.Console.ReadLine();
+		ipcon.Disconnect();
+	}
+}
---
+++ software/examples/csharp/ExampleThreshold.cs
@@ -0,0 +1,36 @@
+using Tinkerforge;
+
+class Example
+{
+	private static string HOST = "localhost";
+	private static int PORT = 4223;
+	private static string UID = "ABC"; // Change to your UID
+
+	// Callback for voltage smaller than 5V
+	static void ReachedCB(BrickletAnalogInV2 sender, int voltage)
+	{
+		System.Console.WriteLine("Voltage dopped below 5V: " + voltage/1000.0 + "V");
+	}
+
+	static void Main() 
+	{
+		IPConnection ipcon = new IPConnection(); // Create IP connection
+		BrickletAnalogInV2 ai = new BrickletAnalogInV2(UID, ipcon); // Create device object
+
+		ipcon.Connect(HOST, PORT); // Connect to brickd
+		// Don't use device before ipcon is connected
+
+		// Get threshold callbacks with a debounce time of 10 seconds (10000ms)
+		ai.SetDebouncePeriod(10000);
+
+		// Register threshold reached callback to function ReachedCB
+		ai.VoltageReached += ReachedCB;
+
+		// Configure threshold for "smaller than 5V" (unit is mV)
+		ai.SetVoltageCallbackThreshold('<', 5*1000, 0);
+
+		System.Console.WriteLine("Press enter to exit");
+		System.Console.ReadLine();
+		ipcon.Disconnect();
+	}
+}
