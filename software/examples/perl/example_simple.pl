e499d3120e15bd5eabca324c79eaa4da81ac33a4
Matthias Bolte <matthias@tinkerforge.com>
2015-08-19 15:38:12 +0200

    Cleanup Perl examples

--- software/examples/perl/example_simple.pl
+++ software/examples/perl/example_simple.pl
@@ -5,7 +5,7 @@ use Tinkerforge::BrickletAnalogInV2;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => 'ABC'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
 my $ai = Tinkerforge::BrickletAnalogInV2->new(&UID, $ipcon); # Create device object
@@ -14,8 +14,8 @@ $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
 
 # Get current voltage (unit is mV)
-my $voltage = $ai->get_voltage()/1000.0;
-print "Voltage: $voltage V\n";
+my $voltage = $ai->get_voltage();
+print "Voltage: " . $voltage/1000.0 . " V\n";
 
 print "Press any key to exit...\n";
 <STDIN>;
