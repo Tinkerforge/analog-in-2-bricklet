834972b058a03a7b273e33792ad8ef16a2dd4335
Matthias Bolte <matthias@tinkerforge.com>
2017-04-21 12:16:26 +0200

    shell: Adapt example

--- software/examples/shell/example-threshold.sh
+++ software/examples/shell/example-threshold.sh
@@ -10,7 +10,7 @@ tinkerforge call analog-in-v2-bricklet $uid set-debounce-period 10000
 tinkerforge dispatch analog-in-v2-bricklet $uid voltage-reached &
 
 # Configure threshold for voltage "smaller than 5 V" (unit is mV)
-tinkerforge call analog-in-v2-bricklet $uid set-voltage-callback-threshold smaller 5000 0
+tinkerforge call analog-in-v2-bricklet $uid set-voltage-callback-threshold threshold-option-smaller 5000 0
 
 echo "Press key to exit"; read dummy
 
